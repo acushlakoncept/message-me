4095874c5ea9270e13c0195ef64425ee32061b3a
Uduak Essien <acushla4real@gmail.com>
2020-08-08 15:00:31 +0100

    add messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,6 +2,8 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
+    @message = Message.new
     @messages = Message.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
