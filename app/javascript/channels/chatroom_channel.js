e2bdb84888011d80037b18c05b46cccb393494c0
Uduak Essien <acushla4real@gmail.com>
2020-08-08 15:52:09 +0100

    modify and broadcast messages

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,6 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    alert(data.foo);
   }
 });
